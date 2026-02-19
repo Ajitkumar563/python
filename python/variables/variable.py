ed1c92b18388069709554184ec439d0404a045f5
AjitKumar <Ajitkm8@gmail.com>
2026-02-19 14:30:27 +0530

    Updated xyz feature

---
+++ python/variables/variable.py
@@ -0,0 +1,13 @@
+"""
+Rules to name a variable
+
+1. Should start with a-z A-Z _
+2. can cantain a-z A-Z _ 0-9
+3. cannot contain symbols $^*#$^#*&
+4. Cannot have resereved keywords (print,for, in, break)
+
+"""
+a=5
+b=10
+c=a+b
+print(c)
