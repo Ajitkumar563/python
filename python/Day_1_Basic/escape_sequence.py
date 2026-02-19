3a7e0b207eb1585ab8684e2c8de2ca6033946e5d
AjitKumar <Ajitkm8@gmail.com>
2026-02-19 14:05:54 +0530

    Updated xyz feature

--- python/Day_1_Basic/escape_sequence.py
+++ python/Day_1_Basic/escape_sequence.py
@@ -11,3 +11,9 @@
 print("Hello world\nAjit and Debug\nWe are learning python")
 print("Hello world\t\tAjit and Debug\t\tWe are learning python")
 
+# A\nirudh
+print("A\\nirudh")
+
+# A\\nirudh
+print("A\\\\nirudh")
+
