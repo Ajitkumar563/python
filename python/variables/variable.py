ad8de669d01288478e705724f69480a1ee1a802c
AjitKumar <Ajitkm8@gmail.com>
2026-02-22 19:33:24 +0530

    Added placeholder files

--- python/variables/variable.py
+++ python/variables/variable.py
@@ -4,7 +4,7 @@ Rules to name a variable
 1. Should start with a-z A-Z _
 2. can cantain a-z A-Z _ 0-9
 3. cannot contain symbols $^*#$^#*&
-4. Cannot have resereved keywords (print,for, in, break)
+4. Cannot have resereved keywords (print,for, in, break)..
 
 """
 a=5
