1bab5249fb8d4a482532c5d6471c31a56798eaee
AjitKumar <Ajitkm8@gmail.com>
2026-02-22 19:42:59 +0530

    Added placeholder files

---
+++ python/variables/printing_variable.py
@@ -0,0 +1,14 @@
+name = "Ajit"
+age = 18
+gender = "male"
+
+# Method 1
+# print("My name is", name)
+# print("My age is", age)
+# print("My gender is",gender)
+# print("My name is", name,"and my age is", age, "and gender is", gender)
+
+#Method 2(F-String)
+print(f"My name is {name}")
+print(f"My age is {age}")
+print(f"My gender is {gender}")
