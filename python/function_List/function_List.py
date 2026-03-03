0b7841d47950fa278c8abe5bc31adb233770b5e8
AjitKumar <Ajitkm8@gmail.com>
2026-03-03 14:44:07 +0530

    Added placeholder files

---
+++ python/function_List/function_List.py
@@ -0,0 +1,11 @@
+ # Write a function named celsius_to_fahrenheit that converts Celsius to Fahrenheit and prints the result.
+ # (Formula: (Celsius * 9/5) + 32 = Fahrenheit)
+ 
+def celsius_to_farenheit(Celsius):
+    
+    fahrenheit = (Celsius * 9/5) + 32 
+    print(fahrenheit)
+ 
+ 
+a=celsius_to_farenheit(45)
+print(a) 
