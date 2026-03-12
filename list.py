321698b725189914b7b2de18856bd24308d9d7fe
AjitKumar <Ajitkm8@gmail.com>
2026-03-12 16:34:35 +0530

    message

--- list.py
+++ list.py
@@ -9,4 +9,18 @@
     
 # print(number)    
 
+# Remove duplicate numbers
+
+# numbers = [1, 6, 5, 1, 1, 1, 10, 1, 6]
+# unique = []
+
+# for i in numbers:
+#     if i not in unique:
+#         unique.append(i)
+        
+# print(unique)        
+
+
+#  Ask 10 numbers from the user and put them into the list. Now remove
+#   all the even numbers from that list.
 
