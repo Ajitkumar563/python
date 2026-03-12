874381c41cffee83a6b399b779a9a241ca7c007f
AjitKumar <Ajitkm8@gmail.com>
2026-03-12 16:20:46 +0530

    python list input program

---
+++ list.py
@@ -0,0 +1,12 @@
+# n  = int(input("enter the no ="))
+
+# number = []
+
+# # Loop
+# for i in range(n):
+#     num = int(input("Enter the number ="))
+#     number.append(num)
+    
+# print(number)    
+
+
