d31ddbcd2eb476e53515aa3623fe7aa3cfab09bb
AjitKumar <Ajitkm8@gmail.com>
2026-03-14 20:16:25 +0530

    message

--- python/Dictionary/Dict.py
+++ python/Dictionary/Dict.py
@@ -169,6 +169,9 @@
 # Output:
 # The character with the maximum frequency is 'l'.
 
+
+
+
 # Q10. Write a Python program to combine two dictionary by adding values
 # for common keys.d1 = {'a': 100, 'b': 200, 'c':300}d2 = {'a': 300, 'b': 200, 'd':400}
 # Sample output: {'a': 400, 'b': 400, 'd': 400, 'c': 300}
