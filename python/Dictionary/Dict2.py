1776655cb67a1c2e85a2d76d435e2a3a7ae16210
AjitKumar <Ajitkm8@gmail.com>
2026-03-17 22:59:49 +0530

    message

--- python/Dictionary/Dict2.py
+++ python/Dictionary/Dict2.py
@@ -5,5 +5,5 @@ details ={
     "Ram": [38,45,67,78,50],
     "Ajeet": [30,40,67,78,57],
 }
-for name, marks in details.items():
-    print(f"Name = {name} | Marks ={sum(marks)}")
+# for name, marks in details.items():
+#     print(f"Name = {name} | Marks ={sum(marks)}")
