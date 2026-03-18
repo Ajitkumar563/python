f2feb5c79aab311877fc4ab2ce5149e7a9e304aa
AjitKumar <Ajitkm8@gmail.com>
2026-03-18 11:04:36 +0530

    message

---
+++ python/Dictionary/sorting_list_in_dictionary.py
@@ -0,0 +1,8 @@
+details = {
+    "Ajit" : {"math" : 23, "english" : 54, "hindi" : 98, "sst" : 43},
+    "Ram" : {"math" : 20, "english" : 50, "hindi" : 88},
+    "ram" : {"math" : 25, "english" : 55, "hindi" : 90},
+    "om" : {"math" : 23, "english" : 54},
+}
+
+print(details.items())
