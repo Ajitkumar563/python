38ae3977f336fd5d5e4287980da8562c93163e8b
AjitKumar <Ajitkm8@gmail.com>
2026-03-18 11:31:18 +0530

    message

--- python/Dictionary/sorting_list_in_dictionary.py
+++ python/Dictionary/sorting_list_in_dictionary.py
@@ -1,16 +1,16 @@
 details = {
-    "Ajit" : {"math" : 23, "english" : 54, "hindi" : 98, "sst" : 43},
-    "Ram" : {"math" : 20, "english" : 50, "hindi" : 88},
-    "ram" : {"math" : 25, "english" : 55, "hindi" : 90},
-    "om" : {"math" : 23, "english" : 54},
+    "Ajit" : [23, 45, 76, 87, 77],
+    "Ram" : [20, 47, 76, 89, 87],
+    "ram" : [30, 48, 96, 97, 77],
+    "om" : [20, 40, 77, 85,27],
 }
 
 # print(details.items())
-x = dict(sorted(details.items(), key =lambda x : sum(x[1])))
+x = dict(sorted(details.items(), key =lambda x: sum(x[1])))
 
 # x = dict(sorted(details.items(), key=lambda x: x[1][-1]))
 # x = dict(sorted(details.items(), key = lambda x: max(x[1])))
 
 print(x)
 for name, marks in x.items():
-    print(f"Name = {name}, marks ={sum(marks)}")
+    print(f"Name = {name}, marks = {sum(marks)}")
