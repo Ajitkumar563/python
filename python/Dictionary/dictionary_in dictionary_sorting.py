e528ca05d71cb493f3c1fc6e0dd1e0f2b8822057
AjitKumar <Ajitkm8@gmail.com>
2026-03-18 12:02:52 +0530

    message

---
+++ python/Dictionary/dictionary_in dictionary_sorting.py
@@ -0,0 +1,10 @@
+details = {
+    "Ajit" : {"math" : 23, "english" : 54, "hindi" : 98, "sst" : 43},
+    "Ram" : {"math" : 20, "english" : 50, "hindi" : 88},
+    "ram" : {"math" : 25, "english" : 55, "hindi" : 90},
+    "om" : {"math" : 23, "english" : 54},
+}
+
+
+print(details.items())
+# x =dict(sorted(details.items(),key=lambda x: x[1]))
