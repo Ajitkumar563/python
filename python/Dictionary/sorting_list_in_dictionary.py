c209715389edc118f090da356165d3c81715daac
AjitKumar <Ajitkm8@gmail.com>
2026-03-18 11:24:56 +0530

    message

--- python/Dictionary/sorting_list_in_dictionary.py
+++ python/Dictionary/sorting_list_in_dictionary.py
@@ -6,11 +6,11 @@ details = {
 }
 
 # print(details.items())
-# x = dict(sorted(details.items(), key =lambda x : sum(x[1])))
+x = dict(sorted(details.items(), key =lambda x : sum(x[1])))
 
 # x = dict(sorted(details.items(), key=lambda x: x[1][-1]))
-x = dict(sorted(details.items(), key = lambda x: max(x[1])))
+# x = dict(sorted(details.items(), key = lambda x: max(x[1])))
 
 print(x)
-# for name, marks in x.items():
-#     print(f"Name = {name}, marks ={sum(marks)}")
+for name, marks in x.items():
+    print(f"Name = {name}, marks ={sum(marks)}")
