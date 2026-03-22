4acb56d823a081c58e375a87f649da4a38a272e2
AjitKumar <Ajitkm8@gmail.com>
2026-03-22 12:18:38 +0530

    message

--- python/Dictionary/Sorting_Assignment.py
+++ python/Dictionary/Sorting_Assignment.py
@@ -1,11 +1,11 @@
  # Q1: Sort dictionary by value (ascending & descending)
  
-d = {1: 2, 4: 3, 2: 1, 0: 0}
+# d = {1: 2, 4: 3, 2: 1, 0: 0}
 
-#Ascending 
-asc = dict(sorted(d.items(), key=lambda x :x[1]))
-print(asc)
+# #Ascending 
+# asc = dict(sorted(d.items(), key=lambda x :x[1]))
+# print(asc)
 
-desc = dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
-print(desc)
+# desc = dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
+# print(desc)
  
