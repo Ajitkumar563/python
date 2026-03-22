32d3b5ca054f6aa2352e56e766633e8991465305
AjitKumar <Ajitkm8@gmail.com>
2026-03-22 12:14:54 +0530

    message

--- python/Dictionary/Sorting_Assignment.py
+++ python/Dictionary/Sorting_Assignment.py
@@ -5,4 +5,7 @@ d = {1: 2, 4: 3, 2: 1, 0: 0}
 #Ascending 
 asc = dict(sorted(d.items(), key=lambda x :x[1]))
 print(asc)
+
+desc = dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
+print(desc)
  
