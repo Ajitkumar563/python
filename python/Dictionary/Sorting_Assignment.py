59d42ab7c7ee90dd5dacbde0969521c2688318c8
AjitKumar <Ajitkm8@gmail.com>
2026-03-22 12:19:54 +0530

    message

--- python/Dictionary/Sorting_Assignment.py
+++ python/Dictionary/Sorting_Assignment.py
@@ -9,3 +9,11 @@
 # desc = dict(sorted(d.items(), key=lambda x: x[1], reverse=True))
 # print(desc)
  
+# Q2. Write a Python program to count number of items in a dictionary value that is a list.
+
+d = {
+    "m1": [67, 79, 90, 73, 36],
+    "m2": [89, 67, 84],
+    "m3": [82, 57]
+}  
+ 
